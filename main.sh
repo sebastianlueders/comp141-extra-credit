fb33a123509aeea1fded47ef844688e231bb87b2
sebastian <sebastianjlueders@gmail.com>
2023-12-07 20:02:44 +0000

    shell function v1.0

--- main.sh
+++ main.sh
@@ -2,3 +2,21 @@
 
 
 mkdir output;
+mv poem.txt output;
+cd output;
+cat poem.txt > read.txt;
+pwd > pwd.txt;
+ls > ls.txt;
+cp poem.txt copy.txt;
+alias current_date="date + "%d-%m-%y";
+current_date > date.txt;
+wc -w poem.txt > textcount.txt;
+ps | head -n 5 > process.txt;
+ifconfig | head -n 5 > netstat.txt;
+mount | head -n 5 > mount.txt;
+touch permissions.txt;
+chmod 777 permissions.txt;
+TESTENV1="test";
+grep -E '^[[:alpha:]]{3,}' poem.txt > regex.txt;
+cd ..;
+
